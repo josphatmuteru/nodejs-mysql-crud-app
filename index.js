5f6b88a193006c6c7b6e26dffa28f1e290110cf0
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-05-05 13:42:07 +0300

    added CRUD operations

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import mysql from 'mysql'
 const app = express()
 const port = 5000
-
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 const dbConnection = mysql.createConnection({
     user: "root",
     database: 'users',
@@ -40,8 +41,53 @@ app.get('/', (req, res)=>{
 
 
 //create
-
+app.post('/create-user', (req, res)=>{
+    console.log(req.body)
+    const { id, username, password, email}= req.body
+    let sql =`insert into userinfo(username,email, password) values('${username}','${email}', '${password}')`
+    dbConnection.query(sql,(err,result)=>{
+        if(!result){
+           res.send(err)
+        }else {
+            res.json({
+                data:result
+            })
+        }
+    })
+})
 //update
+app.put('/update', (req, res)=>{
+    console.log(req.body)
+    const {id, username, password, email} = req.body
+    let sql = `update userinfo set username = '${username}' , password = '${password}' , email = '${email}' where id = ${id} `
+
+    dbConnection.query(sql, (err, result)=>{
+        if(!result){
+            res.send(err)
+        }else{
+            res.json({
+                data:result
+            })
+        }
+    })
+})
+
 //delete
+app.delete('/delete', (req, res)=>{
+    console.log(req.body)
+    const {id}=  {body}
+    let sql = `delete  from userinfo where id = ${id} `
+
+    dbConnection.query(sql, (err, result)=>{
+        if(!result){
+            res.send(err)
+        }else{
+            res.json({
+                data:result
+            })
+        }
+    })
+})
+
 app.listen(port, console.log(`server is running on port ${port}`))
 
